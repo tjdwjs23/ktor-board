5c1b9522821d915e984a75b41ce9e58528c1d718
Seonghun <tjdwjs23@sugarbricks.ne.kr>
2025-03-21 12:56:25 +0900

    feat(ktor) : ktor init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val coroutines_version: String by project
+val java_sdk_version: String by project
+val mysql_version = "8.0.33"
+val hikari_version = "6.2.1"
+val ktorm_version = "4.1.1"
 
 plugins {
     kotlin("jvm") version "2.1.20"
@@ -21,11 +27,24 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty")
+    // Ktor Framework
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-jackson:$ktor_version")
+    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+
+    // Logging
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-test-host")
+
+    // Ktorm ORM
+    implementation("org.ktorm:ktorm-core:$ktorm_version")
+    implementation("org.ktorm:ktorm-jackson:$ktorm_version")
+    implementation("org.ktorm:ktorm-support-mysql:$ktorm_version")
+
+    // MySQL Connector and HikariCP
+    implementation("mysql:mysql-connector-java:$mysql_version")
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,9 +1,38 @@
 package board.ktor
 
 import io.ktor.server.application.*
+import kotlinx.coroutines.runBlocking
+import org.ktorm.database.Database
+import org.ktorm.dsl.from
+import org.ktorm.dsl.select
+import org.ktorm.schema.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+object Departments : Table<Nothing>("t_department") {
+    val id = int("id").primaryKey()
+    val name = varchar("name")
+    val location = varchar("location")
+}
+
+object Employees : Table<Nothing>("t_employee") {
+    val id = int("id").primaryKey()
+    val name = varchar("name")
+    val job = varchar("job")
+    val managerId = int("manager_id")
+    val hireDate = date("hire_date")
+    val salary = long("salary")
+    val departmentId = int("department_id")
+}
+
+//fun main(args: Array<String>) {
+//    io.ktor.server.netty.EngineMain.main(args)
+//}
+
+fun main() {
+    val database = Database.connect("jdbc:mysql://localhost:3306/template", user = "user", password = "user123")
+
+    for (row in database.from(Employees).select()) {
+        println(row[Employees.name])
+    }
 }
 
 fun Application.module() {
